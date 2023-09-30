7ea6944f4bc4e4d5929d9ca58aaa35d3e1c14eee
mphomolefe730 <mphomolefe730@gmail.com>
2023-09-30 04:59:18 +0200

    made side menu

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapPageComponent } from './map-page/map-page.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ChatComponent } from './chat/chat.component';
+import { FloatComponent } from './float/float.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapPageComponent,
+    PaymentComponent,
+    ChatComponent,
+    FloatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
